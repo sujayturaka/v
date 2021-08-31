c6fdc2cb88e4c54fbc641ced0c1aa888ccff271b
sujayturaka <86346460+sujayturaka@users.noreply.github.com>
2021-08-31 15:22:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas=createCanvas(550, 550);
+    canvas.position(560, 150);
+    
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses); 
+    }
+    function modelLoaded() {
+        console.log('PoseNet Is Initialized!');
+    }
+
+    function gotPoses(results)
+    {
+        if (results.length > 0)
+        {
+            console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+
+         leftWristX = results[0].pose.leftWrist.x;
+         rightWristX = results[0].pose.rightWrist.x;
+         difference = floor(leftWristX - rightWristX);
+        }
+    }
+    
+    function draw() {
+        background('#233067')
+
+        textSize(10);
+        fill('#A57164')
+        text('Sujay', 50, 400)
+    }
+   
